ce35db4002849f1222791c9f82e8b4ce71bd8994
Gewalt <gewalt70@gmail.com>
2023-12-29 18:13:25 +0100

    dddd

--- acceuil.php
+++ acceuil.php
@@ -23,16 +23,7 @@
     <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
       <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Gestion de Tickets SYSCAB</h1>
       <p class="mb-8 leading-relaxed">Plateforme de Gestion de Tickets. Pour toutes remarques, veuillez contacter Lucas.</p>
-        <!-- <div class="flex justify-center">
-          <a href='http://syscab.fr/home/support/' <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Création de tickets</button></a>
-          <?php     
-          // if ($_SESSION['admin'] == 1)  {
-          //   echo '<a href="creauser.php" <button class="ml-4 inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Ajouter un utilisateur</button></a>';
-          // }
-          // else{}
-          ?>
-          <a href='aide.php' <button class="ml-4 inline-flex text-gray-700 bg-gray-200 border-0 py-2 px-6 focus:outline-none hover:bg-gray-300 rounded text-lg">Aide</button></a>
-        </div> -->
+
     </div>
     <!-- <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
        <img class="object-cover object-center rounded" alt="hero" src="img/portail.png"> 
