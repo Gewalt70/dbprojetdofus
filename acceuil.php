05b9b140561ac32435ea45a02f315bbd102e0374
Gewalt <gewalt70@gmail.com>
2023-12-29 18:10:41 +0100

    tdsqdsqsd

--- acceuil.php
+++ acceuil.php
@@ -1,9 +1,5 @@
 <?php
-  //header('Location:connexion.php');
-
   include 'navbar.php';
-  include 'pdoexterne.php';
-
   //Fonction pour connaitre le nbr de semaines
   function nbrSemaines()  {
     $today = date("Y-m-d"); 
@@ -25,10 +21,7 @@
 <section class="text-gray-700 body-font">
   <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
     <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-      <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Gestion de Tickets SYSCAB</h1><?php
-      while($ligne = $req->fetch()) {//affichages des heures durant la semaines
-        echo '<p class="title-font sm:text-3xl text-2xl mb-4 font-medium text-gray-900">Cette semaine vous avez signalé ' . $ligne['heuresemaine'] . 'H</p>';
-      }?>
+      <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Gestion de Tickets SYSCAB</h1>
       <p class="mb-8 leading-relaxed">Plateforme de Gestion de Tickets. Pour toutes remarques, veuillez contacter Lucas.</p>
         <!-- <div class="flex justify-center">
           <a href='http://syscab.fr/home/support/' <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Création de tickets</button></a>
