016333055d18c0fcea702460bab489810558e514
Gewalt <gewalt70@gmail.com>
2024-01-07 19:40:20 +0100

    hjkxcvzerhjuk

--- creauser.php
+++ creauser.php
@@ -38,8 +38,8 @@
  
     if($count == 0) {
       
-      $insert=$pdo->prepare("INSERT INTO utilisateurs (email, mdp, pseudo, admin) VALUES (:email, :mdp, :pseudo, :admin)");
-      $insert->execute(array(':email' => $email, ':mdp' => $hashpassword, ':pseudo' => $pseudo, ':admin' => 0));
+      $insert=$pdo->prepare("INSERT INTO utilisateurs (email, mdp, pseudo, admin) VALUES (:email, :mdp, :pseudo, 0)");
+      $insert->execute(array(':email' => $email, ':mdp' => $hashpassword, ':pseudo' => $pseudo));
       echo'<p>Pas de correspondance trouvé alors insert</p>';
     } else {
       echo'<p>Correspondance trouvé alors retour acceuil</p>';
